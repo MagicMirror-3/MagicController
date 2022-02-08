894e61fc12e40f9fab6dd2d8ca3b1943c7982b3a
n1klasD <67094918+n1klasD@users.noreply.github.com>
2022-02-08 12:13:42 +0100

    added timings and Mirror Output

--- FaceRecognition/OptimizedRecognition.py
+++ FaceRecognition/OptimizedRecognition.py
@@ -2,6 +2,7 @@ import cv2 as cv
 import time
 import face_recognition as fr
 import imutils
+from threading import Timer
 
 import numpy as np
 
@@ -15,10 +16,10 @@ class FaceAuth:
         self.users = []
 
     def register_face(self, name, image):
-        # multiple ?
+        # use first face it can find
         self.users.append((name, fr.face_encodings(image)[0]))
 
-    def match_face(self, image, location, tolerance=0.5):
+    def match_face(self, image, location, tolerance=0.55):
         unknown_encoding = fr.face_encodings(image, known_face_locations=[location])[0]
 
         distances = []
@@ -31,12 +32,70 @@ class FaceAuth:
             return "unknown", -1
 
 
+class MirrorFaceOutput:
+    def __init__(self):
+        self.recognized_faces = dict()
+        self.timeout = 5
+        self.currentIdentifiedFace = None
+
+    def face_detected(self, user_name):
+        if user_name != self.currentIdentifiedFace:
+            print(f"Identified: {user_name}")
+
+            # if the current face is not None, the timers have to be stopped
+            if self.currentIdentifiedFace is not None:
+                # new face detected, stop all ongoing timers
+                for user, timer in self.recognized_faces.items():
+                    timer.cancel()
+
+                # clear dictionary
+                self.recognized_faces.clear()
+
+            # set new detected user
+            self.currentIdentifiedFace = user_name
+
+        # check if a timer is already in the dictionary
+        if user_name in self.recognized_faces:
+            # stop timer
+            self.recognized_faces[user_name].cancel()
+        # create a new timer
+        self.recognized_faces[user_name] = Timer(self.timeout, self.face_timeout, args=[user_name])
+
+    def no_faces(self):
+        # start timer(s) for each face, when no face is detected
+        for user, timer in self.recognized_faces.items():
+            try:
+                timer.start()
+            except:
+                pass
+
+    def face_timeout(self, user):
+        # face no longer detected
+        print(f"Face from {user} no longer detected")
+        self.recognized_faces.pop(user)
+        self.currentIdentifiedFace = None
+
+        # different face detected --> not none : stop timeout of all existing timers
+
+
 # -----------------------------------------------------------------------------------
+
+def detected_unknown_face():
+    global detect_anyways
+    detect_anyways = False
+
 
 capture = cv.VideoCapture(0)
 face_detector = cv.CascadeClassifier(cv.data.haarcascades + 'haarcascade_frontalface_default.xml')
 
-number_faces_last_frame = 0
+buffer_size = 15
+face_count_buffer = [0] * buffer_size
+last_number_faces = 0
+
+detect_anyways = False
+timer = None
+
+output = MirrorFaceOutput()
 
 # register faces
 auth = FaceAuth()
@@ -48,7 +107,7 @@ while True:
     start = time.time()
 
     # scale image down
-    frame = imutils.resize(frame, width=500)
+    # frame = imutils.resize(frame, width=500)
 
     frame_gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
     frame_rgb = cv.cvtColor(frame, cv.COLOR_BGR2RGB)
@@ -61,22 +120,35 @@ while True:
     # need to do a bit of reordering
     face_locations = [(y, x + w, y + h, x) for (x, y, w, h) in face_locations]
 
-    # only check identity if number of faces has changed compared to last frame
-    if number_faces_last_frame != len(face_locations):
+    # buffer number of faces
+    face_count_buffer.append(len(face_locations))
+    face_count_buffer.pop(0)
+    number_of_faces = max(face_count_buffer, key=face_count_buffer.count)
 
-        # when there are multiple faces, only identify the first one
-        for face_location in face_locations:
+    # only check identity if number of faces has changed in last X frames
+    if number_of_faces != last_number_faces or detect_anyways:
+        detect_anyways = False
+        if number_of_faces != 0:
+            # when there are multiple faces, only identify the first one, if this is unknown, identify the second one.
+            for face_location in face_locations:
+                # identify face
+                name = auth.match_face(frame_rgb, location=face_location)
+                if name != ('unknown', -1):
+                    # output to mirror
+                    output.face_detected(name[0])
 
-            # identify face
-            name = auth.match_face(frame_rgb, location=face_location)
+                    last_number_faces = number_of_faces
+                    break
 
-            if name != ('unknown', -1):
-                print(f"Identified: {name}")
-                number_faces_last_frame = len(face_locations)
-                break
+            # no faces identified, after x seconds, identify again 
+            if not timer is None and not timer.is_alive():
+                timer = Timer(2, detected_unknown_face)
+                timer.start()
         else:
-            # no faces detected
-            number_faces_last_frame = len(face_locations)
+            # !! should get executed, when no faces are identified  --> not 0 faces detected
+            # only unknown faces identified --> wait one second before trying again ?
+            last_number_faces = number_of_faces
+            output.no_faces()
 
     # draw rectangles for faces
     for f1, f2, f3, f4 in face_locations:
