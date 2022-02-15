efd2f04b2af84acc9564bff493150c9d5ff1bd9f
n1klasD <67094918+n1klasD@users.noreply.github.com>
2022-02-15 21:40:54 +0100

    added test script

---
+++ FaceRecognition/MobileNetwork/Benchmark.py
@@ -0,0 +1,13 @@
+"""
+20 images of ten people
+
+split train / test set
+1/19 --> Leave on out Cross-Validation
+
+test 1: use all testing data, how accurate is prediction ? --> Matrix
+
+test 2: increase number of images to train (1-3), leave number of testing data constant
+    - How does cross validation look like here?
+
+test 3: test X random people, that have not been trained with
+"""
