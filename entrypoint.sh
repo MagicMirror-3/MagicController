58008831f3ac2216ff7b6267a63b974b3963a559
n1klasD <67094918+n1klasD@users.noreply.github.com>
2022-03-15 23:00:21 +0100

    rename + shell logs

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,6 @@
 cd /
 mkdir logs
 python /MagicController/load_default_config.py
-python /MagicController/MagicController.py > /logs/controller_log.txt &
+python /MagicController/MagicController.py &> /logs/controller_log.txt &
 cd /MagicMirror || exit
-npm run server > /logs/mirror_log.txt &
+npm run server &> /logs/mirror_log.txt &
