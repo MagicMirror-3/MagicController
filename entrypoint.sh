230279af0731f1437221a99113b14ea783beae27
n1klasD <67094918+n1klasD@users.noreply.github.com>
2022-03-15 14:51:39 +0100

    added logs

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-
+cd /
+mkdir logs
 cd /MagicMirror || exit
-npm run server &> mirror_log.txt &
-python /MagicController/MagicController.py &> controller_log.txt &
+npm run server &> /logs/mirror_log.txt &
+python /MagicController/MagicController.py &> /logs/controller_log.txt &
 
