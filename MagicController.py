9764e83d08d215db83220a9b8a9dbd15050f1d17
n1klasD <67094918+n1klasD@users.noreply.github.com>
2022-03-15 21:12:14 +0100

    Init with the base config of user 0

--- MagicController.py
+++ MagicController.py
@@ -25,6 +25,9 @@ class MagicController(Mediator):
         self.communication_handler = CommunicationHandler(self)
         self.face_authentication = FaceAuthentication(benchmark_mode=True, lite=True, resolution=(640, 480), mediator=self)
 
+        # init with the base config, which is user 0
+        ConfigurationHandler.updateConfiguration(0)
+
     '''
         self._currentUser = User()
         self._configurationHandler = ConfigurationHandler()
