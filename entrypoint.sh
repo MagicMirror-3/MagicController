d698c82e28c5bc209b570938be17b20bc3f1aab1
n1klasD <67094918+n1klasD@users.noreply.github.com>
2022-03-15 22:06:20 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd /
 mkdir logs
-cd /MagicMirror || exit
 python /MagicController/Configuration/load_default_config.py
 python /MagicController/MagicController.py > /logs/controller_log.txt &
+cd /MagicMirror || exit
 npm run server > /logs/mirror_log.txt &
