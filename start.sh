7714301164f2100ace21567631fa7355720e56d0
n1klasD <67094918+n1klasD@users.noreply.github.com>
2022-04-28 16:24:15 +0200

    added screen rotating command

--- start.sh
+++ start.sh
@@ -2,5 +2,7 @@
 # content: SUBSYSTEM=="vchiq",MODE="0666"
 
 
-docker run -it --privileged=true -v /opt/vc:/opt/vc --env LD_LIBRARY_PATH=/opt/vc/lib --device /dev/vchiq tf-lite:0.1
-docker run -it --privileged=true -v /opt/vc:/opt/vc --env LD_LIBRARY_PATH=/opt/vc/lib --device /dev/vchiq --net=host mirror:latest bash
+# docker run -it --privileged=true -v /opt/vc:/opt/vc --env LD_LIBRARY_PATH=/opt/vc/lib --device /dev/vchiq tf-lite:0.1
+
+xrandr --output HDMI-1 --rotate left
+docker run -it --privileged=true -v /opt/vc:/opt/vc --env LD_LIBRARY_PATH=/opt/vc/lib --device /dev/vchiq --net=host mirror:latest
