c83c2614134c35c163c2c2db33daf348ed5b4498
n1klasD <67094918+n1klasD@users.noreply.github.com>
2022-05-26 16:31:12 +0200

    Update install.sh

--- setup/install.sh
+++ setup/install.sh
@@ -1,4 +1,5 @@
 sudo cp /home/pi/MagicController/setup/magicmirror.service /etc/systemd/system/magicmirror.service
 sudo chmod 644 /etc/systemd/system/magicmirror.service
+sudo chmod +x /home/pi/MagicController/setup/autostart.sh
 sudo systemctl enable magicmirror.service
 sudo systemctl start magicmirror.service
