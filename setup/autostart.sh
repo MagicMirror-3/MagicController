f9a65f4fd821e9e4e5c83205ff7b86d4fb23b681
n1klasD <67094918+n1klasD@users.noreply.github.com>
2022-05-26 16:36:28 +0200

    Update autostart.sh

--- setup/autostart.sh
+++ setup/autostart.sh
@@ -1,3 +1,3 @@
 xrandr --output HDMI-1 --rotate left
 docker run -t -d --privileged=true -v /opt/vc:/opt/vc --env LD_LIBRARY_PATH=/opt/vc/lib --device /dev/vchiq --net=host mirror2:latest
-chromium-browser --kiosk --app=localhost:8080
+chromium --kiosk --app=localhost:8080
