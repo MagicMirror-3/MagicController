16e7692c95da948aac7df7dd5c5568bfe5f4f8da
n1klasD <67094918+n1klasD@users.noreply.github.com>
2022-05-31 21:17:22 +0200

    added install fuctionality script

--- setup/install.sh
+++ setup/install.sh
@@ -1,3 +1,21 @@
+sudo apt update -y
+sudo apt upgrade -y
+cd /home/pi || exit
+git clone https://github.com/n1klasD/MagicController.git
+cd /home/pi/MagicController || exit
+
+# install docker
+sudo curl -s https://get.docker.com | bash
+sudo usermod -aG docker pi
+
+# pull docker image
+docker pull 1646552/magic-controller:latest
+
+# add camera rules
+sudo cp setup/99-camera.rules /etc/udev/rules.d/99-camera.rules
+# create the file /etc/udev/rules.d/99-camera.rules with content: SUBSYSTEM=="vchiq",MODE="0666"
+
+# Add to autostart
 sudo cp /home/pi/MagicController/setup/magicmirror.service /etc/systemd/system/magicmirror.service
 sudo chmod 644 /etc/systemd/system/magicmirror.service
 sudo chmod +x /home/pi/MagicController/setup/autostart.sh
