cda339648d8e76ea5f9db9f2fd94acde4908b167
n1klasD <67094918+n1klasD@users.noreply.github.com>
2022-06-01 19:49:48 +0200

    added bind mount for code directory

--- setup/autostart.sh
+++ setup/autostart.sh
@@ -1,3 +1,11 @@
 xrandr --output HDMI-1 --rotate left
-docker run -t -d --privileged=true -v /opt/vc:/opt/vc --env LD_LIBRARY_PATH=/opt/vc/lib --device /dev/vchiq --net=host 1646552/magic-controller:latest
+docker run -t \
+  -d \
+  --privileged=true \
+  -v /opt/vc:/opt/vc \
+  --env LD_LIBRARY_PATH=/opt/vc/lib \
+  --device /dev/vchiq \
+  --net=host \
+  --mount type=bind,source=/home/pi/MagicController,target=/MagicController \
+  1646552/magic-controller:latest
 chromium --kiosk http://localhost:8080
