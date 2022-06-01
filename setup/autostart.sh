84386b1d22db88b55a6ef0aa4c83b958f7da42ad
n1klasD <67094918+n1klasD@users.noreply.github.com>
2022-06-01 15:40:20 +0200

    .

--- setup/autostart.sh
+++ setup/autostart.sh
@@ -1,3 +1,3 @@
 xrandr --output HDMI-1 --rotate left
-docker run -t -d --privileged=true -v /opt/vc:/opt/vc --env LD_LIBRARY_PATH=/opt/vc/lib --device /dev/vchiq --net=host magic-controller:latest
+docker run -t -d --privileged=true -v /opt/vc:/opt/vc --env LD_LIBRARY_PATH=/opt/vc/lib --device /dev/vchiq --net=host 1646552/magic-controller:latest
 chromium --kiosk http://localhost:8080
