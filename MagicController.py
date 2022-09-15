01f2fb6d93975509a55eb016d9eb05602049ebfd
Stausssi <stauss.simon.01@gmail.com>
2022-09-15 10:48:35 +0200

    Logging for MagicController

--- MagicController.py
+++ MagicController.py
@@ -1,5 +1,6 @@
 import sys
 from abc import ABC
+from loguru import logger
 
 from Communication import CommunicationHandler
 from Configuration import ConfigurationHandler
@@ -23,6 +24,8 @@ class BaseComponent:
 
 class MagicController(Mediator):
     def __init__(self, host):
+        logger.info("Starting the MagicController...")
+
         self.communication_handler = CommunicationHandler(self, host)
         self.face_authentication = FaceAuthentication(benchmark_mode=False,
                                                       lite=True,
@@ -36,6 +39,8 @@ class MagicController(Mediator):
 
         # init with the base config, which is user 0
         ConfigurationHandler.updateConfiguration(self.current_user_id)
+
+        logger.success("MagicController started!")
 
     '''
         self._currentUser = User()
@@ -87,6 +92,8 @@ class MagicController(Mediator):
             None: Nothing
         """
 
+        logger.debug(f"Swapping layout from {self.current_user_id} to {user_id}")
+
         # Keep track of the current user and update the layout if the config was updated
         if ConfigurationHandler.updateConfiguration(user_id):
             self.current_user_id = user_id
@@ -94,6 +101,8 @@ class MagicController(Mediator):
             self.communication_handler.refresh_layout()
 
     def notify(self, sender: object, *args) -> None:
+        logger.debug(f"Notification from {sender} with the arguments {args}")
+
         # communication handler sends request to register a user
         if isinstance(sender, CommunicationHandler.CreateUser):
             callback = args[0]
@@ -103,15 +112,20 @@ class MagicController(Mediator):
             # Call FaceAuthentication to register faces
             success = self.face_authentication.register_faces(user_id, images, min_number_faces=4, mode='slow')
 
+            logger.debug(f"New user created: {success}")
             # call callback, to send the response to the http server.
             callback(success)
 
+            return
+
         # Face Recognition detected a face
         if isinstance(sender, MirrorFaceOutput):
-            print("call notify: face detected")
             detected_user_id: int = 0 if args[0] is None else int(args[0])
+            logger.debug(f"User {detected_user_id} detected!")
 
             self._loadUserLayout(detected_user_id)
+
+            return
 
         # A user is deleted
         if isinstance(sender, CommunicationHandler.DeleteUser):
@@ -123,7 +137,9 @@ class MagicController(Mediator):
             if user_id == self.current_user_id:
                 self._loadUserLayout(0)
 
-            print(f"Deleted user: {user_id}")
+            logger.debug(f"Deleted user: {user_id}")
+
+            return
 
         # Layout is updated of a user
         if isinstance(sender, CommunicationHandler.SetLayout):
@@ -133,12 +149,20 @@ class MagicController(Mediator):
             if user_id == self.current_user_id:
                 self._loadUserLayout(user_id)
 
+            return
 
+        logger.warning("This notification was not handled correctly!")
+
+@logger.catch
 def main():
     try:
         host = sys.argv[1]
     except IndexError:
         host = None
+
+    # Init the logger with a new file every day
+    logger.add("logs/{time:YYYY:MM:DD}.log", rotation="12:00", enqueue=True, backtrace=True, diagnose=True)
+
     MagicController(host)
 
 
